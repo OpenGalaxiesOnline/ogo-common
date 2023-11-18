7a3f566f393629edc91d123747c1724bde9890fa
Dylan Legendre <dylanlegendre09@gmail.com>
2023-11-18 10:40:55 -0600

    adding user interface

---
+++ src/interfaces/base-entity.interface.ts
@@ -0,0 +1,6 @@
+export interface IBaseEntity {
+  id: number;
+  created: Date;
+  modified: Date;
+  deleted: Date;
+}
--- src/interfaces/user.interface.ts
+++ src/interfaces/user.interface.ts
@@ -1,12 +1,12 @@
-export interface IUser {
-  id: number;
-  created: Date;
-  modified: Date;
-  deleted: Date;
+import { IBaseEntity } from "./base-entity.interface";
+
+export interface IUser extends IBaseEntity {
   email: string;
   hash: string;
   email_verified: boolean;
-  banned_until: Date;
-  banned_message: string;
-  user_type: string;
+  user_role: string;
+  banned_until?: Date;
+  banned_message?: string;
+  allow_newsletter: boolean;
+  last_login: Date;
 }
